ed94c6506610593f4ba402abbe2c9553d0c09a50
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2022-02-10 10:45:29 -0300

    DATABASE

--- Procfile
+++ Procfile
@@ -1,4 +1,3 @@
-heroku run python manage.py migrate
 release: py manage.py migrate
 web: gunicorn tourproject.wsgi --preload --log-file -
 python manage.py collectstatic --noinput
--- tourproject/settings/heroku.py
+++ tourproject/settings/heroku.py
@@ -7,7 +7,6 @@ env = environ.Env()
 
 DEBUG = env.bool('DEBUG', False)
 
-# SECRET_KEY = env('SECRET_KEY')
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
