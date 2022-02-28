208cd69a2fe1f75792de8f82f1796903a967c5ff
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2022-02-28 18:18:39 -0300

    create_user

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 web: gunicorn tourproject.wsgi --preload --log-file -
-python3 manage.py migrate
+
 release: python3 manage.py migrate
 python3 manage.py collectstatic --noinput
--- user/managers.py
+++ user/managers.py
@@ -2,10 +2,20 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import UserManager
 
 
-class DefaultUserManager(UserManager):
+class DefaultUserManager(UserManager):        
+        
+    def create_user(self, email, password=None, is_staff=False, is_superuser=False, is_active=True):
 
-    def create_user(self, *args, **kwargs):
-        super().create_user(*args, **kwargs)
+        user_obj = self.model(
+            email=self.normalize_email(email),
+        )
+        user_obj.set_password(password)
+        user_obj.username = email
+        user_obj.is_active = is_active
+        user_obj.is_staff = is_staff
+        user_obj.is_superuser = is_superuser
+        user_obj.save(using=self._db)
+        return user_obj
 
     def create_superuser(self, email, password=None):
         user = self.create_user(
