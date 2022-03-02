292b596466078bd4c99e9c0b7f1744ed75580ef5
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2022-03-02 09:00:38 -0300

    ajustes create_user

--- user/managers.py
+++ user/managers.py
@@ -2,15 +2,16 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import UserManager
 
 
-class DefaultUserManager(UserManager):        
-        
-    def create_user(self, email, password=None, is_staff=False, is_superuser=False, is_active=True):
+class DefaultUserManager(UserManager):
+
+    def create_user(self, email, option, password=None, is_staff=False, is_superuser=False, is_active=True):
 
         user_obj = self.model(
             email=self.normalize_email(email),
         )
         user_obj.set_password(password)
         user_obj.username = email
+        user_obj.option = option
         user_obj.is_active = is_active
         user_obj.is_staff = is_staff
         user_obj.is_superuser = is_superuser
@@ -19,7 +20,6 @@ class DefaultUserManager(UserManager):
 
     def create_superuser(self, email, password=None):
         user = self.create_user(
-            username=email,
             email=email,
             password=password,
             option='0',
