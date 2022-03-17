97b49105c7c6affe52f7ddf50c3fcb035464a8c9
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2022-03-17 08:20:14 -0300

    ajustes

--- destiny/admin.py
+++ destiny/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Destiny
 
-
-@admin.register(Destiny)
 class DestinyAdmin(admin.ModelAdmin):
-    ... 
+    list_display = ('season','destiny', 'state','city','date_start','date_finish', 'active', 'description')
+    
+admin.site.register(Destiny, DestinyAdmin)   
