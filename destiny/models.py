774594ad0e4c3d742581687acf6bbdb7a8a9adaf
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2022-04-01 14:27:21 -0300

    estados destino

--- destiny/models.py
+++ destiny/models.py
@@ -1,10 +1,42 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 
 class Destiny(models.Model):
 
+    STATE_CHOICES = [
+        ('AC', 'AC'),
+        ('AL', 'AL'),
+        ('AP', 'AP'),
+        ('AM', 'AM'),
+        ('BA', 'BA'),
+        ('CE', 'CE'),
+        ('DF', 'DF'),
+        ('ES', 'ES'),
+        ('GO', 'GO'),
+        ('MA', 'MA'),
+        ('MT', 'MT'),
+        ('MS', 'MS'),
+        ('MG', 'MG'),
+        ('PA', 'PA'),
+        ('PB', 'PB'),
+        ('PE', 'PE'),
+        ('PI', 'PI'),
+        ('PR', 'PR'),
+        ('RJ', 'RJ'),
+        ('RN', 'RN'),
+        ('RO', 'RO'),
+        ('RR', 'RR'),
+        ('RS', 'RS'),
+        ('SC', 'SC'),
+        ('SE', 'SE'),
+        ('SP', 'SP'),
+        ('TO', 'TO'),
+    ]
+
     name = models.CharField('Destino', max_length=255,)
-    state = models.CharField('Estado', max_length=2)
+    state = models.CharField(_('Estado'), choices=STATE_CHOICES, max_length=2)
     city = models.CharField('Cidade',
                             max_length=100,
                             null=True,
