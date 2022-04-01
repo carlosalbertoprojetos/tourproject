1c6877fda609fcdee7a6a55971091f8d81bcb0f3
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2022-04-01 12:03:40 -0300

    ajustes

--- season/urls.py
+++ season/urls.py
@@ -12,19 +12,19 @@ from .views import (
 app_name = 'season'
 
 urlpatterns = [   
-    path('validity/list/', validity_list, name='validity_list'),
-    path('validity/create/', validity_create, name='validity_create'),
-    path('<int:pk>/validity/edit/', validity_update, name='validity_update'),
-    path('<int:pk>/validity/delete/', validity_delete, name='validity_delete'),
-    
+
     path('list/', seasons_list, name='seasons_list'),
     path('create/', season_create, name='season_create'),
     path('<int:pk>/edit/', season_update, name='season_update'),
     path('<int:pk>/delete/', season_delete, name='season_delete'),
+    
+    path('validity/list/', validity_list, name='validity_list'),
+    path('validity/create/', validity_create, name='validity_create'),
+    path('<int:pk>/validity/edit/', validity_update, name='validity_update'),
+    path('<int:pk>/validity/delete/', validity_delete, name='validity_delete'),
 
     path('period/list/', period_list, name='period_list'),
     path('period/create/', period_create, name='period_create'),
     path('<int:pk>/option/edit/', period_update, name='period_update'),
     path('<int:pk>/option/delete/', period_delete, name='period_delete'),
-
 ]
