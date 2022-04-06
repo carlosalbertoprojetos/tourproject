53bd0bd8bff1d71dd0f48f8f6e0e7cf301c8f902
Flaviobh <flavio.piunibh@gmail.com>
2022-04-06 15:14:47 -0300

    atualizando main

--- tourproject/settings/base.py
+++ tourproject/settings/base.py
@@ -192,12 +192,15 @@ STATIC_URL = '/static/'
 
 
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_ROOT = BASE_DIR / 'static'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'basics', 'static'),
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
