14d317ad3dd7e0a34e1263455f3861081b4a9ec1
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2022-04-08 19:39:13 -0300

    'atualização'

--- tourproject/settings/base.py
+++ tourproject/settings/base.py
@@ -136,13 +136,13 @@ DATABASES = {
     }
 }
 
-# if 'DATABASE_URL' in os.environ:
-#     import dj_database_url
-#     DATABASES = {'default': dj_database_url.config()}
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
 
 # utilizado pelo heroku
-#db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
