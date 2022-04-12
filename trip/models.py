e388dd6d8da5c0437bcd83fdea48b5393a17b8bb
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2022-04-11 21:05:13 -0300

    'opções_passeio'

--- trip/models.py
+++ trip/models.py
@@ -1,14 +1,10 @@
 from company.models import Company
 from django.db import models
-from season.models import Season
-
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # from django.utils.html import mark_safe
-# from django.utils.text import slugify
-
-
-
+from django.utils.text import slugify
+from season.models import Season
 
 
 class TripCategoryPax(models.Model):
@@ -56,25 +52,25 @@ class Trip(models.Model):
         ('16','16'),
     ]
     
-    name = models.CharField('Nome', max_length=255, default='teste')
-    # slug = models.SlugField(max_length=250)
+    name = models.CharField('Nome', max_length=255)
+    slug = models.SlugField(max_length=250)
     image = models.ImageField(
         'Imagem do produto', upload_to="produtos/%Y", blank=True)
-    trip_description = models.TextField('Descrição do passeio', blank=True, default='teste')
-    short_description = models.TextField('Descrição curta', blank=True, default='teste')
+    trip_description = models.TextField('Descrição do passeio', blank=True)
+    short_description = models.TextField('Descrição curta', blank=True)
     
     politic = models.CharField('Política de CHD', choices=CHD_CHOICES, max_length=2)
-    trip_duration = models.CharField('Duração do passeio (hrs)', max_length=255, default='1')
+    trip_duration = models.CharField('Duração do passeio (hrs)', max_length=255)
     
-    travel_time = models.CharField('Tempo de percurso (hrs)', max_length=255, default='00:00')
-    travel_time_untoplace = models.CharField('Tempo de percurso até o local do passeio (hrs)', max_length=255, default='00:00')
+    travel_time = models.CharField('Tempo de percurso (hrs)', max_length=255)
+    travel_time_untoplace = models.CharField('Tempo de percurso até o local do passeio (hrs)', max_length=255)
     
-    ride_distance = models.CharField('Distância do passeio (Km)', max_length=255, default='1')
-    limit_load = models.CharField('Limite de carga por passeio ou guia (Nº de pessoas)', max_length=255, default='6')
-    commission = models.DecimalField('Comissão paga pelo fornecedor (%)', max_digits=5, decimal_places=2, blank=True, null=True, default='10')
+    ride_distance = models.CharField('Distância do passeio (Km)', max_length=255)
+    limit_load = models.CharField('Limite de carga por passeio ou guia (Nº de pessoas)', max_length=255)
+    commission = models.DecimalField('Comissão paga pelo fornecedor (%)', max_digits=5, decimal_places=2, blank=True, null=True)
     category = models.ForeignKey(TripCategory, on_delete=models.CASCADE, verbose_name='Categoria')
     company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='Empresa')
-    tour_notes = models.TextField('Notas do passeio', blank=True, default='teste')
+    tour_notes = models.TextField('Notas do passeio', blank=True)
     featured_image = models.FileField('Imagem de destaque para o site', upload_to='files/')
     
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
@@ -88,25 +84,54 @@ class Trip(models.Model):
     # @property
     # def view_image(self):
     #     return mark_safe('<img src="%s" width="400px" />'%self.imagem.url)
-        # view_image.short_description = "Imagem Cadastrada"
-        # view_image.allow_tags = True
+    #     view_image.short_description = "Imagem Cadastrada"
+    #     view_image.allow_tags = True
 
     def __str__(self):
         return self.name
 
 
-# @receiver(post_save, sender=Trip)
-# def insert_slug(sender, instance, **kwargs):
-#     if not instance.slug or instance.slug != slugify(instance.name):
-#         instance.slug = slugify(instance.name)
-#         return instance.save()
- 
+@receiver(post_save, sender=Trip)
+def insert_slug(sender, instance, **kwargs):
+    if not instance.slug or instance.slug != slugify(instance.name):
+        instance.slug = slugify(instance.name)
+        return instance.save()
+
+
+class TripOption(models.Model):
+    
+    SCALE_CHOICE = [
+        ('1','1'),
+        ('2','2'),
+        ('3','3'),
+        ('4','4'),
+        ('5','5'),
+        ('6','6'),
+        ('7','7'),
+        ('8','8'),
+        ('9','9'),
+        ('10','10'),
+    ]
+    
+    name = models.CharField('Nome', max_length=255)
+    description = models.TextField('Descrição do passeio', blank=True)
+    min_amount_pax = models.IntegerField('Quantidade mínima PAX')
+    occ_scale = models.CharField('Escala de Ocupação diária (1 a 10)', max_length=2, choices=SCALE_CHOICE)
+    tariff_group = models.BooleanField('A tarifa é de Grupo',)
+    customer_option = models.BooleanField('A opção pode ser selecionada pelos clientes nos sites?',)
+    night_walk = models.BooleanField(' O passeio é realizado somente no período noturno?',)
+    
+    
+    def __str__(self):
+        return self.name
+
 
 class TripPrice(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.DO_NOTHING, verbose_name='Passeio')
+    trip_option = models.ForeignKey(TripOption, on_delete=models.DO_NOTHING, verbose_name='Opção de Passeio')
     season = models.ForeignKey(Season, on_delete=models.DO_NOTHING, verbose_name='Temporada')
     cadpax = models.ForeignKey(TripCategoryPax, on_delete=models.DO_NOTHING, verbose_name='Cadastro PAX')
     price = models.CharField('Preço', max_length=9)
 
     def __str__(self):
-        return self.trip +' - '+ self.season +' - '+ self.cadpax +' - R$ '+ self.price
+        return self.trip +' - '+ self.trip_option +' - '+ self.season +' - '+ self.cadpax +' - R$ '+ self.price
--- trip/urls.py
+++ trip/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (trip_category_delete, trip_category_list_create,
                     trip_category_update, trip_categorypax_delete,
                     trip_categorypax_list_create, trip_categorypax_update,
-                    trip_delete, trip_list_create, trip_price_delete,
+                    trip_delete, trip_list_create, trip_option_list_create, trip_option_update, trip_option_delete, trip_price_delete,
                     trip_price_list_create, trip_price_update, trip_update)
 
 app_name = 'trip'
@@ -26,7 +26,13 @@ urlpatterns = [
     # PASSEIO
     path('list/', trip_list_create, name='trip_list_create'),
     path('<int:pk>/edit/', trip_update, name='trip_update'),
-    path('<int:pk>/delete/', trip_delete, name='trip_delete'),
+    path('<int:pk>/delete/', trip_delete, name='trip_delete'),  
+    
+    #===============================================================================
+    # OPÇÕES DE PASSEIO
+    path('options/list/', trip_option_list_create, name='trip_option_list_create'),
+    path('<int:pk>/option/edit/', trip_option_update, name='trip_option_update'),
+    path('<int:pk>/option/delete/', trip_option_delete, name='trip_option_delete'),
 
     #===============================================================================
     # PREÇOS DOS PASSEIOS
