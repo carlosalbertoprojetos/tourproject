6e20870ff9ac55381f56d4a42f6c11ace360b746
Flaviobh <flavio.piunibh@gmail.com>
2022-05-02 21:12:20 -0300

    ajustes calendario

--- basics/static/js/mascara.js
+++ basics/static/js/mascara.js
@@ -1,5 +1,5 @@
 
-$(function(){
+function mascara (){
     $('.mask-cpf').mask('000.000.000-00', {reverse: true});
     $('.mask-cnpj').mask('00.000.000/0000-00', {reverse: true});
     $('.mask-telefone').mask(' (00)00000-0000', {reverse: true});
@@ -8,4 +8,4 @@ $(function(){
     $('.mask-hora').mask('00:00', {reverse: true});
     $('.mask-perc').mask('00.00', {reverse: true});
     $('.mask-real').mask({prefix:'R$ ', reverse: true});
-});
+};
