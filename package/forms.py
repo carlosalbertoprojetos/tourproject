93731b549c34c183a8e91ddc98b4a8b699cc2c59
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2022-06-28 10:22:52 -0300

    'opções_para_inclusão_de_campo_dinâmico_no_formset'

--- package/forms.py
+++ package/forms.py
@@ -37,6 +37,7 @@ class Data_Package_OneForm(forms.ModelForm):
                         
             'num_child':forms.NumberInput(
                 attrs={
+                    # 'oninput':'campos();',
                     # 'oninput':'camposParcela();',
                     'class':'form-control my-2',
                     # 'label':'Quantidade de Crianças',
