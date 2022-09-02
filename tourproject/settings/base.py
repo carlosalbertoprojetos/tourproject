ed2f2a96617f13e24da5050f503a98d3cfb1d89a
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2022-09-02 10:48:34 -0300

    make it better

--- tourproject/settings/base.py
+++ tourproject/settings/base.py
@@ -192,7 +192,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 
 
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 #STATIC_ROOT = BASE_DIR / 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'basics', "static")
 
