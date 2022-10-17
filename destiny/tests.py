8965f8fe96b3b0e56bf7485aec0cc98729038708
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2022-10-17 14:40:58 -0300

    'inclusão_de_testes_destiny'

--- destiny/tests.py
+++ destiny/tests.py
@@ -1,3 +1,25 @@
+from unicodedata import name
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Destiny
+
+
+class TesteCreateDestiny(TestCase):
+    
+    def setUp(self):
+        Destiny.objects.create(
+        name = 'Minas',
+        state = 'MG',
+        city = 'Ibié'
+        )
+        
+    def test_novo_destino_criado(self):
+        destiny = Destiny.objects.get(name='Minas')
+        self.assertEquals(destiny.__str__(), 'Minas: Ibié/MG')
+        self.assertEquals(destiny.city_state(), 'Ibié/MG')
+
+        print(
+            'Nome:', destiny.name,'\n'
+            'Estado:', destiny.state,'\n'
+            'Cidade:', destiny.city,'\n'
+        )
