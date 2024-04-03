409900191282298fda18635ccbc6b1b365696e2c
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2024-04-03 15:13:00 -0300

    diversos ajsutes de layout

--- client/views.py
+++ client/views.py
@@ -12,11 +12,11 @@ from .models import Client
 
 class ClientListCreateView(LoginRequiredMixin, SuccessMessageMixin, ListView):
     model = Client
-    template_name = 'client/client_list_create.html'
-    
+    template_name = "client/client_list_create.html"
+
     def get_context_data(self, **kwargs):
         context = super(ClientListCreateView, self).get_context_data(**kwargs)
-        context['form'] = ClientForm(self.request.POST or None)
+        context["form"] = ClientForm(self.request.POST or None)
         return context
 
     def post(self, request, *args, **kwargs):
@@ -24,10 +24,15 @@ class ClientListCreateView(LoginRequiredMixin, SuccessMessageMixin, ListView):
 
         if form.is_valid():
             form.save()
-            messages.success(request, 'Transporte criado com sucesso!!!')
-            return redirect('client:client_list_create')
+            messages.success(request, "Transporte criado com sucesso!!!")
+            return redirect("client:client_list_create")
         else:
-            return render(request, 'client/client_list_create.html', {'object':'object','form': form})
+            return render(
+                request,
+                "client/client_list_create.html",
+                {"object": "object", "form": form},
+            )
+
 
 client_list_create = ClientListCreateView.as_view()
 
@@ -35,20 +40,22 @@ client_list_create = ClientListCreateView.as_view()
 class ClientUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Client
     form_class = ClientForm
-    template_name = 'client/client_edit.html'
-    success_message = 'Dados do cliente alterados com sucesso!!!'
-    success_url = _('client:client_list_create')
+    template_name = "client/client_update.html"
+    success_message = "Dados do cliente alterados com sucesso!!!"
+    success_url = _("client:client_list_create")
+
 
 client_update = ClientUpdateView.as_view()
 
 
 class ClientDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Client
-    template_name = 'client/client_delete.html'
-    success_message = 'Cliente deletado com sucesso!'
-    success_url = _('client:client_list_create')
+    template_name = "client/client_delete.html"
+    success_message = "Cliente deletado com sucesso!"
+    success_url = _("client:client_list_create")
 
-    def delete(self, request, *args, **kwargs):        
+    def delete(self, request, *args, **kwargs):
         return super(ClientDeleteView, self).delete(request, *args, **kwargs)
+
 
 client_delete = ClientDeleteView.as_view()
