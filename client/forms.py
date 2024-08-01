f33ab7590bb019e6b0c8140aff43d8ee5030f849
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2024-08-01 15:51:20 -0300

    correções de layout

--- client/forms.py
+++ client/forms.py
@@ -1,10 +1,31 @@
 from django import forms
 
+
 from .models import Client
 
-
 class ClientForm(forms.ModelForm):
- 
     class Meta:
         model = Client
-        fields = '__all__'
+        fields = [
+            'name',
+            'cpf',
+            'street',
+            'number',
+            'complement',
+            'postal_code',
+            'state',
+            'city',
+            'email',
+            'phoneNumber',
+            'is_active',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(ClientForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field.widget.attrs.get('class'):
+                field.widget.attrs['class'] += ' form-control form-control-sm'
+            else:
+                field.widget.attrs['class'] = 'form-control form-control-sm'
+
+# Abaixo está como você pode usar esse formulário na sua template
